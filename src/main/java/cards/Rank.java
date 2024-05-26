37c802971aed3d192252022e6d33155f69b90d02
laptop1 <gmacc.bpf@gmail.com>
2024-05-26 18:42:25 +0200

    Aufgaben 1-4 bearbeitet; nr 4 ? wg zustansdiagramm und zuständen

--- src/main/java/cards/Rank.java
+++ src/main/java/cards/Rank.java
@@ -4,7 +4,19 @@ package cards;
  * Represents the ranks of playing cards.
  */
 public enum Rank {
-    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE;
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE;
 
     /**
      * Returns a string representation of the rank.
@@ -13,7 +25,30 @@ public enum Rank {
      */
     @Override
     public String toString() {
-        //TODO implement
-        return super.toString();
+        try {
+            return switch (this) {
+                case TWO -> "2";
+                case THREE -> "3";
+                case FOUR -> "4";
+                case FIVE -> "5";
+                case SIX -> "6";
+                case SEVEN -> "7";
+                case EIGHT -> "8";
+                case NINE -> "9";
+                case TEN -> "10";
+                case JACK -> "J";
+                case QUEEN -> "Q";
+                case KING -> "K";
+                case ACE -> "A";
+            };
+        }
+        catch(Exception e){
+            try {
+                throw new Exception(e);
+            }
+            catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        }
     }
 }
